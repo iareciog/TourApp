c7683d90305248a2f80fc58113a5a8e3d1129837
MusicPlayer <ignacio.recio.gomez@gmail.com>
2018-04-27 21:10:00 +0200

    Some changes

--- app/src/main/java/com/example/android/tourapp/InfoActivity.java
+++ app/src/main/java/com/example/android/tourapp/InfoActivity.java
@@ -3,7 +3,6 @@ package com.example.android.tourapp;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.TextView;
 
 public class InfoActivity extends AppCompatActivity {
@@ -16,7 +15,7 @@ public class InfoActivity extends AppCompatActivity {
         Intent i = getIntent();
 
         int zoneName = i.getIntExtra("zoneName", 0);
-        int zoneText = i.getIntExtra("zoneText",0);
+        int zoneText = i.getIntExtra("zoneText", 0);
 
         TextView textZoneName = findViewById(R.id.info_title);
         TextView textZone = findViewById(R.id.info_text);
